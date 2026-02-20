7409f26e710ebf0360b7b17a2166d2f5b819fa92
angelcj <angelcjchirayath136@gmail.com>
2026-02-20 13:04:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/ai-feedback-app",
+  base: process.env.VITE_BASE_PATH || "/AI-feedback-App",
   build: {
     outDir: 'dist'
   }
